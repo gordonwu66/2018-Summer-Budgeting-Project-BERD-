4f3ae0e4d7564ed8178252d0ab8e830c513fd58e
Jeffrey Li <jeffreyhli951@gmail.com>
2018-06-21 15:42:27 -0400

    Entry class

---
+++ Entry.java
@@ -0,0 +1,48 @@
+import java.util.GregorianCalendar;
+
+public class Entry {
+	private String name, notes;
+	private int cost;
+	private GregorianCalendar cal;
+	
+	public Entry(String name, String notes, int cost, int year, int month, int day) {
+		this.name = name;
+		this.notes = notes;
+		this.cost = cost;
+		cal = new GregorianCalendar(year, month, day);
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+	
+	public String getNotes() {
+		return notes;
+	}
+	
+	public void setCost(int cost) {
+		this.cost = cost;
+	}
+	
+	public int getCost() {
+		return cost;
+	}
+	
+	public void setDate(int year, int month, int day) {
+		cal.set(year, month, day);
+	}
+	
+	public GregorianCalendar getDate() {
+		return cal;
+	}
+	
+	
+}
