dcec642f0d72bb31967871b9122b15c0751babec
kdubbs0 <kwwong410@gmail.com>
2018-06-25 10:19:44 -0400

    spreadsheet update 1

---
+++ Spreadsheet.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.GregorianCalendar;
+// import java.util.HashMap;
+
+public class Spreadsheet {
+	// HashMap<GregorianCalendar, ArrayList<Entry>> allEntries;
+	ArrayList<Entry> allEntries;
+	ArrayList<Category> categories;
+	
+	public Spreadsheet() {
+		// allEntries = new HashMap<GregorianCalendar, ArrayList<Entry>>();
+		allEntries = new ArrayList<Entry>();
+		categories = new ArrayList<Category>();
+	}
+	
+	public void addEntry(Entry newEntry) {
+		allEntries.add(newEntry);
+		Collections.sort(allEntries);
+	}
+	
+	public Entry removeEntry() {
+		Entry entry = null;
+		
+		// fix me
+		for (Entry temp : allEntries.values()) {
+			
+		}
+		
+		return entry;
+	}
+	
+	public void addCategory(Category newCategory) {
+		categories.add(newCategory);
+	}
+	
+	public Category removeCategory() {
+		Category category = null;
+		
+		for (Category temp : categories) {
+			
+		}
+		
+		return category;
+	}
+
+	public String toString() {
+		String str = "";
+		
+		
+		
+		return str;
+	}	
+}
