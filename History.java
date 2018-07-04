ed065cc6bbe6f92f68b27348e17e376641323176
gordonwu66 <gordonwu66@gmail.com>
2018-07-04 00:27:31 -0400

    History update

--- History.java
+++ History.java
@@ -1,5 +1,40 @@
-/* Stores a list of entries in chronological order based on purchase date from
- * tree set of all entries recorded by user */
-public class History {
+import java.util.ArrayList;
 
+/* 
+ * Stores a list of entries in chronological order based on purchase date from
+ * tree set of all entries recorded by user
+ */
+public class History {
+	
+	private String name;
+	private ArrayList<Entry> entries;
+	
+	public History(String nameIn, ArrayList<Entry> entriesIn) {
+		name = nameIn;
+		entries = entriesIn;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String newName) {
+		name = newName;
+	}
+	
+	public ArrayList<Entry> getEntries() {
+		return entries;
+	}
+	
+	public void setEntries(ArrayList<Entry> newEntries) {
+		entries = newEntries;
+	}
+	
+	public String toString() {
+		String toReturn = "";
+		for(Entry e : entries) {
+			toReturn += e.getDate() +" " +e.getName() +" " +e.getCost() +" " +e.getNotes();
+		}
+		return toReturn;
+	}
 }
