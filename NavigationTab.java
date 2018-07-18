69e785792327673515f49aeb4d2f6d38c438c28a
kdubbs0 <kwwong410@gmail.com>
2018-07-18 12:07:23 -0400

    what is happening with navbryce

---
+++ NavigationTab.java
@@ -0,0 +1,17 @@
+
+public class NavigationTab {
+	Page currentPage;
+	
+	// might need one with GUI but idk about eclipse
+	public NavigationTab() {
+		
+	}
+	
+	// a lot of this is in GUI so idk how much i can do in eclipse for this
+	// display selected page
+	public void switchPage(Page page) {
+		currentPage = page;
+		
+		// currentPage.show()?
+	}
+}
